a403a17625acb0aaed65f298bd0f2f14942bf384
Deepali <122445268+lavenderpeaches@users.noreply.github.com>
2025-09-20 00:46:48 +0530

    Create HomePage.jsx to store the content of main page

--- BookMyShow/src/App.jsx
+++ BookMyShow/src/App.jsx
@@ -1,39 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
-import RecommendedMovies from './components/MainContent/RecommendedMovies'
 import Footer from './components/Footer'
-import Banner from './components/MainContent/Banner'
-import Primere from './components/MainContent/Primere'
-import Trending from './components/MainContent/Trending'
-import LiveEvents from './components/MainContent/LiveEvents'
-import MusicStudio from './components/MainContent/MusicStudio'
-import Outdoor from './components/MainContent/Outdoor'
-import LaughterTherapy from './components/MainContent/LaughterTherapy'
-import PopularEvents from './components/MainContent/PopularEvents'
-import LatestPlays from './components/MainContent/LatestPlays'
-import TopGames from './components/MainContent/TopGames'
-import FunActivities from './components/MainContent/FunActivities'
-import Slider from './components/MainContent/Slider'
+import HomePage from './components/HomePage'
 
 function App() {
   
   return (
     <>
       <Header />
-      <Slider />
-      <RecommendedMovies /> 
-      <Banner />
-      <LiveEvents />
-      <Primere />
-      <MusicStudio />
-      <Outdoor />
-      <LaughterTherapy />
-      <PopularEvents />
-      <LatestPlays />
-      <TopGames />
-      <FunActivities />
-      <Trending />
+      <HomePage />
       <Footer />
     </>
   )
